3a465605a7350c69d9a8e1a00cfcaa6722ef9c9d
Zedrick Serson <71012553+sersonz@users.noreply.github.com>
2023-11-12 17:42:26 -0500

    Added model initialization

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import pycuber
 import numpy as np
 from utils import *
+import tensorflow as tf
+from tensorflow import keras
 
 class CubeModel:
 	def __init__(self):
 		self.cube = pycuber.Cube()
 		self.solved_cube = self.cube.copy()
+	    # Make model
+		model_input = keras.Input(shape=(20*24,))
+		first_layer = tf.keras.layers.Dense(4096, activation="relu")(model_input)
+		second_layer = tf.keras.layers.Dense(1024, activation="relu")(first_layer)
+		value_layer = tf.keras.layers.Dense(480, activation="relu")(second_layer)
+		policy_layer = tf.keras.layers.Dense(480, activation="relu")(second_layer)
+		value_output = tf.keras.layers.Dense(1, name="value")(value_layer)
+		policy_output = tf.keras.layers.Dense(12, name="policy")(policy_layer)
+		self.model = tf.keras.Model(inputs=model_input, outputs=[value_output, policy_output])
 	
 	def getState(self):
 		def compare(x):
@@ -29,3 +40,4 @@ class CubeModel:
 			state[i][2*EDGES.index(set(map(lambda key: edge.facings[key].colour, cubes))) + EDGE_COLORS[edge.facings[cubes[0]].colour]] = 1
 			i += 1
 		return state
+	
