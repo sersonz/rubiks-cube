43ef6414f6e80fcd39f4a95e819797b4d3ff3c89
ecal <ethan@ecal.dev>
2023-11-13 18:51:21 -0500

    reorder action order to match paper

--- utils.py
+++ utils.py
@@ -1,46 +1,44 @@
 CORNERS = [
-    {'white', 'orange', 'blue'},
-    {'white', 'orange', 'green'},
-    {'white', 'red', 'green'},
-    {'white', 'red', 'blue'},
-    {'yellow', 'orange', 'blue'},
-    {'yellow', 'orange', 'green'},
-    {'yellow', 'red', 'green'},
-    {'yellow', 'red', 'blue'}
+    {"white", "orange", "blue"},
+    {"white", "orange", "green"},
+    {"white", "red", "green"},
+    {"white", "red", "blue"},
+    {"yellow", "orange", "blue"},
+    {"yellow", "orange", "green"},
+    {"yellow", "red", "green"},
+    {"yellow", "red", "blue"},
 ]
 
 EDGES = [
-    {'white', 'blue'},
-    {'white', 'orange'},
-    {'white', 'green'},
-    {'white', 'red'},
-    {'yellow', 'blue'},
-    {'yellow', 'orange'},
-    {'yellow', 'green'},
-    {'yellow', 'red'},
-    {'orange', 'blue'},
-    {'blue', 'red'},
-    {'green', 'red'},
-    {'orange', 'green'}
+    {"white", "blue"},
+    {"white", "orange"},
+    {"white", "green"},
+    {"white", "red"},
+    {"yellow", "blue"},
+    {"yellow", "orange"},
+    {"yellow", "green"},
+    {"yellow", "red"},
+    {"orange", "blue"},
+    {"blue", "red"},
+    {"green", "red"},
+    {"orange", "green"},
 ]
 CORNER_COLORS = {
-    'white': 0,
-    'yellow': 0,
-    'blue': 1,
-    'green': 1,
-    'orange': 2,
-    'red': 2
+    "white": 0,
+    "yellow": 0,
+    "blue": 1,
+    "green": 1,
+    "orange": 2,
+    "red": 2,
 }
 
 EDGE_COLORS = {
-    'white': 0,
-    'yellow': 0,
-    'blue': 0,
-    'green': 1,
-    'orange': 1,
-    'red': 1
+    "white": 0,
+    "yellow": 0,
+    "blue": 0,
+    "green": 1,
+    "orange": 1,
+    "red": 1,
 }
 
-ACTIONS = ["F", "B", "U", "D", "L", "R", "F'", "B'", "U'", "D'", "L'", "R'"]
-
-	
+ACTIONS = ["F", "B", "L", "R", "U", "D", "F'", "B'", "L'", "R'", "U'", "D'"]
