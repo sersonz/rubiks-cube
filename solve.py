eee509e3567341239efec15c982c53ff7de1321a
ch3llenger <amaliecs99@gmail.com>
2023-11-15 06:30:30 -0500

    mcts

--- solve.py
+++ solve.py
@@ -1,4 +1,62 @@
 """Monte Carlo Tree Search Solver"""
+from utils import ACTIONS
+
+class Node:
+    #constants
+    #c
+   
+    c=1
+    def __init__(self,cube,model,actionInd,parent=None):
+        self.cube = cube
+        self.parent = parent
+        self.model = model
+        self.actionInd = actionInd
+        self.isLeaf = True
+
+        self.w,self.p = model.estimate(cube)
+        self.n=[0]*len(ACTIONS)
+
+        self.l= None
+
+
+    def update(self,w,ind):
+        self.w = max(self.w,w)
+        self.n[ind] += 1
+
+        if self.parent:
+            self.parent.update(w,ind)
+
+    def simulate(self):
+
+        if self.isLeaf:
+            self.isLeaf = False
+            self.children = {  val:Node(cube.copy()(val), model, ind)
+                                   for ind,val in enumerate(ACTIONS) }
+            self.update(self.w, self.actionInd) # back +it should contain L
+            return
+        
+        choice=0
+        value=0
+
+        for ind,val in enumerate(ACTIONS):
+            new_value= self.children[val].w + c *sum(slef.n)**0.5 / (1+self.n[ind])
+            if new_value > value:
+                value = new_value
+                choice = ind
+
+        self.children[choice].simulate()
+
+
+            
+
+
+
+
+
+
+       
+
+
 
 def solve():
     """
